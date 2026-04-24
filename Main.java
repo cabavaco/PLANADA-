cf5d85e3fa3bd1d542f50fdbddb6a7e17b19e347
Adam Mathias <cabavaco1@gmail.com>
2026-04-24 18:24:19 +0300

    Initial commit - Planada project

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("PLANADA - Smart Education Planning System");
+        System.out.println("Teachers can plan:");
+        System.out.println("- Scheme of Work");
+        System.out.println("- Lesson Plans");
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# PLANADA
+
+Smart Education Planning System.
+
+This project helps teachers prepare:
+- Scheme of Work
+- Lesson Plans
+
+Version: 1.0 (Starter)
